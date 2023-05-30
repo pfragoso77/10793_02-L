651343fad1812b048593199309d80e1d941f53df
pfragoso77 <134558690+pfragoso77@users.noreply.github.com>
2023-05-30 23:20:05 +0100

    Add files via upload

---
+++ 1. Exercício de Variáveis e Operações Matemáticas.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 30 22:21:59 2023
+
+@author: Pedro
+"""
+#1. Exercício de Variáveis e Operações Matemáticas
+numero1 = int(input("Digite o primeiro número: "))
+numero2 = int(input("Digite o segundo número: "))
+
+print("A soma dos 2 números é: ", numero1 + numero2)
+print("A subtração dos 2 números é: ", numero1 - numero2)
+print("A multiplicação dos 2 números corresponde a: ", numero1 * numero2)
+print("O resultado da divisão entre os 2 números é:", numero1 // numero2)
