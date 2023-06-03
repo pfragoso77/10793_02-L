0ad81f02b744574a9b7c36bbc766ae0d77283b89
pfragoso77 <134558690+pfragoso77@users.noreply.github.com>
2023-06-03 11:16:05 +0100

    Create 2. Exercício de Estruturas de Controlo.py

---
+++ 2. Exercício de Estruturas de Controlo.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  1 22:56:22 2023
+
+@author: Pedro
+"""
+
+num1 = int(input("Digite um número por favor: "))
+
+if num1 % 2==0:
+    print("O número é par.")
+else:
+    print("O número é ímpar.")
